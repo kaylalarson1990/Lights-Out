14e2c023442beb5b62602c704458e2a89c89ec6c
Kayla Larson <kayla@radialdevgroup.com>
2020-09-18 15:58:01 -0600

    create grid

---
+++ src/components/Game.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import '../App.css';
+
+export default class Game extends React.Component {
+  render() {
+    return (
+      <div className="game-container">
+        <table>
+          <tr>
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+            <td className="game-cubes" />
+          </tr>
+        </table>
+      </div>
+    )
+  }
+}
